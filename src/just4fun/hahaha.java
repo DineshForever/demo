b0ada803c33ca4d5d21bedc74e19c0540b7bdd4c
Vijay Selvaraj <vijayvj7094@gmail.com>
2023-10-03 13:43:41 +0530

    done by vijay

--- src/just4fun/hahaha.java
+++ src/just4fun/hahaha.java
@@ -1,9 +1,20 @@
 package just4fun;
 
+import java.util.Iterator;
+
 public class hahaha {
 
 	public static void main(String[] args) {
 		System.out.println("poda da");
+		
+		String obj="WELCOME";
+		
+		for (int i = 0; i <obj.length(); i++) {
+			System.out.println(obj.charAt(i));
+			
+		}
+		
+				
 
 	}
 
